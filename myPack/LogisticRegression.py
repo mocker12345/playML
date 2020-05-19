5beaee1c97bef7eca4a95c04aedcff7cef2e89e3
rancongjie <95633676@qq.com>
2020-05-19 14:37:38 +0800

    logistc Decision  tree

--- myPack/LogisticRegression.py
+++ myPack/LogisticRegression.py
@@ -1,37 +1,30 @@
 import numpy as np
-from myPack.metrics import r2_score
+from myPack.metrics import accuracy_score
 
 
-class LinearRegression:
+class LogisticRegression:
 
     def __init__(self):
         self.coef_ = None  # 系数
         self.intercept_ = None  # 截距
         self._theta = None
 
-    def fit_normal(self, X_train, y_train):
-        assert X_train.shape[0] == y_train.shape[0], \
-            "the size of X_train must be equal to the size of y_train"
-        # 列扩充
-        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+    def _sigmoid(self, t):
+        return 1 / (1 + np.exp(-t))
 
-        self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y_train)
-        self.intercept_ = self._theta[0]
-        self.coef_ = self._theta[1:]
-        return self
-
-    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=1e4):
+    def fit(self, X_train, y_train, eta=0.01, n_iters=1e4):
         assert X_train.shape[0] == y_train.shape[0], \
             "the size of X_train must be equal to the size of y_train"
 
         def J(theta, X_b, y):
+            y_hat = self._sigmoid(X_b.dot(theta))
             try:
-                return np.sum((y - X_b.dot(theta)) ** 2) / len(y)
+                return -np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat)) / len(y)
             except:
                 return float('inf')
 
         def dJ(theta, X_b, y):
-            return X_b.T.dot(X_b.dot(theta) - y) * 2. / len(X_b)
+            return X_b.T.dot(self._sigmoid(X_b.dot(theta)) - y) / len(X_b)
 
         def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
             theta = initial_theta
@@ -55,14 +48,18 @@ class LinearRegression:
         self.coef_ = self._theta[1:]
         return self
 
-    def predict(self, X_predict):
+    def predict_proba(self, X_predict):
         X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
-        return X_b.dot(self._theta)
+        return self._sigmoid(X_b.dot(self._theta))
+
+    def predict(self, X_predict):
+        proba = self.predict_proba(X_predict)
+        return np.array(proba >= 0.5, dtype='int')
 
     def score(self, x_test, y_test):
         """根据x_test y_test 确定当前模型的准确度"""
         y_predict = self.predict(x_test)
-        return r2_score(y_test, y_predict)
+        return accuracy_score(y_test, y_predict)
 
     def __repr__(self):
-        return "LinearRegression()"
+        return "LogisticRegression()"
